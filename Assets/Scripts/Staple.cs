d64814fc0d158cf7518321ed1e02a31d22b5ab2e
LuraMoth <fasreaperearnest@gmail.com>
2022-02-28 23:20:32 -0800

    added licence information to scripts

--- Assets/Scripts/Staple.cs
+++ Assets/Scripts/Staple.cs
@@ -2,23 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+    Corperate Corperal, A top down 2D shooter game
+    Copyright (C) 2022  Luramoth
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
+
 public class Staple : MonoBehaviour
 {
 
-    public float stapleSpeed = 1f;
+	public float stapleSpeed = 1f;
 
-    public GameObject stapler;
+	public GameObject stapler;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        transform.position = stapler.transform.position;
-        transform.rotation = stapler.transform.rotation;
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		transform.position = stapler.transform.position;
+		transform.rotation = stapler.transform.rotation;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(Vector3.up * Time.deltaTime * stapleSpeed);
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Translate(Vector3.up * Time.deltaTime * stapleSpeed);
+	}
 }
